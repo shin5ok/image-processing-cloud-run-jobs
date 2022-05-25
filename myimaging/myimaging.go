ecfbf118a865e47b4b1d6537b65eececcbc5ff93
shin5ok <shin5ok@55mp.com>
2022-05-25 22:53:58 +0900

    rename some variable

--- myimaging/myimaging.go
+++ myimaging/myimaging.go
@@ -15,18 +15,18 @@ func (i Image) MakeSmall(width int) (string, error) {
 
 	dirname := filepath.Dir(i.Filename)
 	basename := filepath.Base(i.Filename)
-	newfilename := fmt.Sprintf("%s/%s_%s", dirname, "new", basename)
+	newFilename := fmt.Sprintf("%s/%s_%s", dirname, "new", basename)
 
 	srcImage, err := imaging.Open(i.Filename, imaging.AutoOrientation(true))
 	if err != nil {
 		fmt.Println(err.Error())
 		return "", err
 	}
-	newfilenameImage := imaging.Resize(srcImage, width, 0, imaging.Lanczos)
-	if err := imaging.Save(newfilenameImage, newfilename); err != nil {
+	newFilenameImage := imaging.Resize(srcImage, width, 0, imaging.Lanczos)
+	if err := imaging.Save(newFilenameImage, newFilename); err != nil {
 		fmt.Println(err.Error())
 		return "", err
 	}
 
-	return newfilename, nil
+	return newFilename, nil
 }
